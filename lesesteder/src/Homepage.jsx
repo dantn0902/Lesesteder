2bf8306d883770f44d0f96aaf65def10a9e73d2a
Dan Thanh Ngu <dantn0902@gmail.com>
2025-10-16 15:12:30 +0200

    Add Library object
    Change filename on existing files from Library to Librarypage, add Library object

--- lesesteder/src/Homepage.jsx
+++ lesesteder/src/Homepage.jsx
@@ -1,17 +1,14 @@
 import './Homepage.css'
 import { useNavigate } from 'react-router-dom';
 import bilde1 from "./assets/bilde1.png";
-import bilde2 from "./assets/bilde2.jpg";
-import bilde3 from "./assets/bilde3.jpg";
-import TopBar from './components/TopBar';
+import { Library } from "../Library";
 
 
 export default function Homepage(){
     const navigate = useNavigate();
     const goToLibraryPage = () => {
-    // Naviger til biblioteksiden
-    navigate(`/Library`);
-  };
+        navigate(`/Library`); // Naviger til biblioteksiden
+    };
     return(
         <div className='content'>
             <TopBar></TopBar>
@@ -45,6 +42,16 @@ export default function Homepage(){
                      <h3>IFI bib.</h3>
                      <p>Åpningstid: 8:00 - 16:00</p>
                 </div> 
+                {/*
+                const [libraries, setLibraries] = useState([]);
+                {libraries.map((b, i)=>(
+                    <div className="library-card" onClick={() => goToLibraryPage()}>
+                        <img src={bilde1} width='180px'></img>
+                        <h3>Sophus Bugge bib.</h3> //<BeachCard key={i} beach={b} />
+                        <p>Åpningstid: 8:00 - 16:00</p>
+                    </div> 
+                ))}
+                */}
             </div>
         </div>
     )
---
+++ lesesteder/src/Library.js
@@ -0,0 +1,7 @@
+export class Library {
+    constructor () {
+        this.åpent = 0;
+        this.stengt = 24
+        this.stillesone = true;
+    }
+}
