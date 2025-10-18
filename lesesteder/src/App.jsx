89a66f1366a37322e497d885979a165c3292edb9
Dan Thanh Ngu <dantn0902@gmail.com>
2025-10-18 12:04:27 +0200

    Create Json file
    Create a json file for libraries, for easier storage of library information and move assets to public folder

--- lesesteder/src/App.jsx
+++ lesesteder/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
 import Homepage from './Homepage';
 import Librarypage from './Librarypage';
--- lesesteder/src/components/LibraryCard.jsx
+++ lesesteder/src/components/LibraryCard.jsx
@@ -1,8 +1,6 @@
-import React from 'react';
-
-export default function LibraryCard({ library, onClick }){
+export default function LibraryCard({ library, onClickEvent }){
     return(
-        <div className="library-card" onClick={onClick}>
+        <div className="library-card" onClick={onClickEvent}>
             <img src={library.bilde} width='180px' alt={`${library.navn} bilde`} />
             <h3>{library.navn}</h3>
             <p>Åpningstid: {library.åpent} - {library.stengt}</p>
