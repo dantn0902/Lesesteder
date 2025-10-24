7c8c1f02db2be77bb0c22063facf0972de6c6977
Lan Anh Tran <lananht@hotmail.no>
2025-10-24 12:33:46 +0200

    Prøver å implementere json uten duplikater
    Feilet

--- lesesteder/src/Homepage.jsx
+++ lesesteder/src/Homepage.jsx
@@ -8,7 +8,15 @@ import FilterScreen from "./components/FilterScreen";
 
 export default function Homepage(){
     // Filter
-   const [openFilterScreen, setFilterScreen] = useState(false)
+    const [openFilterScreen, setFilterScreen] = useState(false)
+    const handleFilterScreen = () => {
+            setFilterScreen(!openFilterScreen)
+    }
+
+    // Krav
+    const r = Object.keys(libraries[0]);
+    console.log(r)
+
     // Navigasjon
     const navigate = useNavigate();
     const goToLibraryPage = () => {
@@ -25,10 +33,10 @@ export default function Homepage(){
                 </p>
             </div>  
             <div className='filterButton'>
-                <button onClick={()=> setFilterScreen(!openFilterScreen)}>Filter</button>
+                <button onClick={()=> handleFilterScreen()}>Filter</button>
             </div>
             <div className={`filterScreen ${openFilterScreen ? "active" : ""}`}>
-                <FilterScreen></FilterScreen>
+                <FilterScreen event={()=> handleFilterScreen()} requirements={r}></FilterScreen>
             </div>
             <div className='cards'>
                 {libraries.map((bib) => (
--- lesesteder/src/components/FilterScreen.jsx
+++ lesesteder/src/components/FilterScreen.jsx
@@ -1,7 +1,16 @@
 import "../styles/FilterScreen.css"
 
-export default function FilterScreen() {
+export default function FilterScreen( {event}, requirements ) {
     return (
-        <p>Test</p>
+        <div className="container">
+            <h3>Fasiliteter og kapasitet</h3>
+            
+            <h3>Tjenester og nærområder</h3>
+
+            <h3>Miljø og komfort</h3>
+
+             <button onClick={ event }>Vis</button>
+        </div>
+       
     )
 }
