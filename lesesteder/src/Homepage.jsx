10ac2243563e37d78d3799856b617cadbf7de4d8
Dan Thanh Ngu <dantn0902@gmail.com>
2025-10-24 15:32:27 +0200

    Legger til valgte filtere i array
    Lager funksjon for å legge til filtere i en liste og endrer parameter navn på FilterScreen

--- lesesteder/src/Homepage.jsx
+++ lesesteder/src/Homepage.jsx
@@ -13,15 +13,31 @@ export default function Homepage(){
         navigate(`/Library`);
     };
     
-    // Filter
+    // Filter screen
     const [openFilterScreen, setFilterScreen] = useState(false)
     const handleFilterScreen = () => setFilterScreen(!openFilterScreen);
 
-    // Krav
+    // options for filter
     const r = useMemo(() => {
         console.log("keys computed");
         return Object.keys(librariesData[0]);
     }, []);
+
+    // Filter 
+    const [libraries, setLibraries] = useState(librariesData);
+    const [filtersOn, setFiltersOn] = useState([]); // Array for aa ta vare paa filtere som bruker har trykket
+    const handleFilterChange = (update) => {
+        console.log("Updating filter:", update);
+        console.log("Previous filters:", filtersOn);
+        setFiltersOn(prevFilters => {
+            if (prevFilters.includes(update)) {
+                return prevFilters.filter(filter => filter !== update); // remove option
+            } else {
+                return [...prevFilters, update]; // add option
+            }
+        });
+        console.log("Current filters:", filtersOn);
+    }
 
     return(
         <div className='content'>
@@ -35,10 +51,10 @@ export default function Homepage(){
                 <button onClick={()=> handleFilterScreen()}>Filter</button>
             </div>
             <div className={`filterScreen ${openFilterScreen ? "active" : ""}`}>
-                <FilterScreen event={()=> handleFilterScreen()} requirements={r}></FilterScreen>
+                <FilterScreen onClose={()=> handleFilterScreen()} options={r} onOptionToggle={handleFilterChange}></FilterScreen>
             </div>
             <div className='cards'>
-                {librariesData.map((bib) => (
+                {libraries.map((bib) => (
                     <LibraryCard library={bib} onClickEvent={() => goToLibraryPage()} />
                 ))}
             </div>
--- lesesteder/src/components/FilterScreen.jsx
+++ lesesteder/src/components/FilterScreen.jsx
@@ -1,21 +1,20 @@
 import "../styles/FilterScreen.css"
 
-export default function FilterScreen( {event, requirements} ) {
+export default function FilterScreen( {onClose, options, onOptionToggle} ) {
     return (
         <div className="container">
             <h3>Fasiliteter og kapasitet</h3>
-            {requirements.map((req) => (
-            <label key={req}>
-                <input type="checkbox" value={req} />
-                {req}
-            </label>
+            {options.map((req) => (
+                <label key={req}>
+                    <input type="checkbox" value={req} onChange={(e) => onOptionToggle(e.target.value)}/>
+                    {req}
+                </label>
             ))}
-
             <h3>Tjenester og nærområder</h3>
 
             <h3>Miljø og komfort</h3>
 
-             <button onClick={ event }>Vis</button>
+             <button onClick={ onClose }>Vis</button>
         </div>
        
     )
